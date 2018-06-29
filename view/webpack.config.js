1731128043e94b1c49c9679b1373911f7d772df9
abody amer <abdullha.eshaq94@gmail.com>
2018-06-29 17:18:48 +0800

    s2

---
+++ view/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+    entry: './src/index.js', 
+    output: {
+        path: path.join(__dirname , 'dist'),
+        filename: 'index_bundle.js'
+    }, 
+    module :{
+        rules:[
+            {
+                test: /\.js$/ , 
+                exclude: /node_modules/ , 
+                use:{
+                    loader: 'babel-loader'
+                }
+            }, 
+            {
+                test:/\.css$/,
+                use:{
+                    loader: ['style-loader' , 'css-loader']
+                }
+            }
+        ]
+    },
+    devServer: { 
+        inline: true, 
+        contentBase: './dist', 
+        port: 8080, 
+        proxy: { "/api/**": { target: 'http://localhost:5000', secure: false }  }
+     },
+    plugins:[ new HTMLWebpackPlugin({
+        template: './src/index.html'
+    })]
+
+}
+
+module.exports = config
